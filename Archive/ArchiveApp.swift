008baaa37338513a70d462665c972eb5701370b8
HoYoung Peter Lee <hy.l@kakao.com>
2022-04-01 16:55:19 +0900

    Initial Commit

---
+++ Archive/ArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveApp.swift
+//  Archive
+//
+//  Created by 이호영 on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Archive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Archive
+//
+//  Created by 이호영 on 2022/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
